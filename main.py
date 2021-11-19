e223bf57e7d46cb52e48a226c03eb3c77ac80d6a
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-19 05:35:31 +0000

    Added teacher solution

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ for n in range(0, len(student_heights)):
 
 
 #Write your code below this row 👇\
-total = 0
-for n in range(0, len(student_heights)):
-  total += student_heights[n]
-avg = round(total / len(student_heights))
-print(avg)
+# total = 0
+# for n in range(0, len(student_heights)):
+#   total += student_heights[n]
+# avg = round(total / len(student_heights))
+# print(avg)
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+
+average = round(total_height / number_of_students)
+print(average)
 
